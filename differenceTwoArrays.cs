ce3edbb5f85283edcc2a19e5778563652558ee3a
Pedro Goerck Martin <154913053+martinestudar@users.noreply.github.com>
2026-09-20 22:05:01 -0300

    Implement FindDifference method to compare two arrays

--- differenceTwoArrays.cs
+++ differenceTwoArrays.cs
@@ -1,1 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
 
+public class Solution {
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {
+        HashSet<int> set1 = new HashSet<int>(nums1);
+        HashSet<int> set2 = new HashSet<int>(nums2);
+
+        IList<int> answer1 = new List<int>();
+        IList<int> answer2 = new List<int>();
+
+        foreach (int num in set1) {
+            if (!set2.Contains(num)) {
+                answer1.Add(num);
+            }
+        }
+
+        foreach (int num in set2) {
+            if (!set1.Contains(num)) {
+                answer2.Add(num);
+            }
+        }
+      
+        return new List<IList<int>> { answer1, answer2 };
+    }
+}
